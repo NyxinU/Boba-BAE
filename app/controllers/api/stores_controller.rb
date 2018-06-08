c13ac2aedd9c03f3eeb59cbfb0b21e9782815bed
Nix <yiunixon@gmail.com>
2018-06-08 16:52:33 -0700

    write store controller

---
+++ app/controllers/api/stores_controller.rb
@@ -0,0 +1,55 @@
+class Api::StoresController < ApplicationController
+  require "json"
+  require "http"
+  require "optparse"
+
+  def create
+    @store = Store.new(store_params)
+
+    if @store.save
+      render :show 
+    else 
+      render json: @store.errors.full_messages, status: 422
+    end 
+  end 
+
+  def show
+    @store = Store.find_by(yelp_store_id: params[:id])
+    yelp_store_info = get_store_info(params[:id])
+    
+    if yelp_store_info
+      if @store
+        render json: store.to_json
+      else 
+        create
+      end 
+    else 
+      render json: ["Business does not exist"], status: 404
+    end 
+
+  end 
+
+  private 
+
+  def store_params 
+    params.require(:store).permit(:yelp_store_id)
+  end 
+
+  # Constants, do not change these
+  API_KEY = '467mR1Rp6HH3uwrJdQulLhSstM1SMXY_LJyCKZD_DgVuYPUTVRh9YFbXES9vrUOiVmX32U04PYyMK7c38tzd0JNaIyny_pdVf0dnrJNTI6ak6cpoCwkOl2QlJVqOWnYx'
+  API_HOST = "https://api.yelp.com"
+  SEARCH_PATH = "/v3/businesses/search"
+  BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
+
+  DEFAULT_BUSINESS_ID = "yelp-san-francisco"
+  DEFAULT_TERM = "boba"
+  DEFAULT_LOCATION = "San Francisco, CA"
+  SEARCH_LIMIT = 5
+
+  def get_store_info(yelp_store_id)
+    url = "#{API_HOST}#{BUSINESS_PATH}#{yelp_store_id}"
+    response = HTTP.auth("Bearer #{API_KEY}").get(url)
+    response.parse
+  end 
+
+end
--- app/models/store.rb
+++ app/models/store.rb
@@ -3,26 +3,4 @@ class Store < ApplicationRecord
 
   has_many :drinks
 
-  require "http"
-  require "optparse"
-
-
-  # Constants, do not change these
-  API_KEY = '467mR1Rp6HH3uwrJdQulLhSstM1SMXY_LJyCKZD_DgVuYPUTVRh9YFbXES9vrUOiVmX32U04PYyMK7c38tzd0JNaIyny_pdVf0dnrJNTI6ak6cpoCwkOl2QlJVqOWnYx'
-  API_HOST = "https://api.yelp.com"
-  SEARCH_PATH = "/v3/businesses/search"
-  BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
-
-
-  DEFAULT_BUSINESS_ID = "yelp-san-francisco"
-  DEFAULT_TERM = "boba"
-  DEFAULT_LOCATION = "San Francisco, CA"
-  SEARCH_LIMIT = 5
-
-  def get_store_info(yelp_store_id)
-    url = "#{API_HOST}#{BUSINESS_PATH}#{yelp_store_id}"
-    response = HTTP.auth("Bearer #{API_KEY}").get(url)
-    response.parse
-  end 
-
 end
