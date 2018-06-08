69462b0a217d834c898116d7a4fd37f29696ab16
Nix <yiunixon@gmail.com>
2018-06-08 15:46:35 -0700

    write drink model

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,2 +1,20 @@
 class Drink < ApplicationRecord
+  validates :store_id, :name, presence: true
+
+  belongs_to :store
+
+  # has_many :reviews
+
+  def update_name(name)
+    self.update_attributes(name: name)
+  end
+
+  def update_review_count
+
+  end 
+
+  def update_rating
+
+  end 
+  
 end
