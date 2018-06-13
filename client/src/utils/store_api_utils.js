c2afba0971d04f7bf7719f9548c602dcccff30b8
Nix <yiunixon@gmail.com>
2018-06-13 14:52:47 -0700

    write store api utils

--- client/src/utils/store_api_utils.js
+++ client/src/utils/store_api_utils.js
@@ -2,18 +2,9 @@ import axios from 'axios';
 
 const url = 'http://localhost:3000';
 
-// export const createStore = store => (
-//   axios({
-//     method: 'POST',
-//     url: `${url}/api/stores`,
-//     data: { store },
-//   })
-// );
-
 export const fetchStore = id => (
   axios({
     method: 'GET',
     url: `${url}/api/stores/${id}`,
-    data: { store: id },
   })
 );
