974ba847067fa4f21b80d93d4b0c54ac8d0f4460
Nix <yiunixon@gmail.com>
2018-06-13 17:53:29 -0700

    write store action, util, reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Root from './components/Root';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/store';
-
-// import * as store from './utils/store_api_utils';
-
-// window.fetchStore = store.fetchStore;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
@@ -17,4 +12,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
-registerServiceWorker();
+registerServiceWorker(); 
